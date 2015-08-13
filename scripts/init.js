0c2865e678722bab8c02c6e9471efba7e0be6176
MatthewMob <zwiddi011@ZWIDDI011_2017.hillsgrammar.nsw.edu.au>
2015-08-13 18:19:48 +1000

    Added more CSS

---
+++ scripts/init.js
@@ -0,0 +1,80 @@
+/* Make a 2D array that holds the cards as references - This array will be used to select cards on draws */
+var cardList = [
+	[ // Card Backs
+		"b2fv"
+	],
+	[ // Aces
+		1,
+		2,
+		3,
+		4
+	],
+	[ // 2s
+		49,
+		50,
+		51,
+		52
+	],
+	[ // 3s
+		45,
+		46,
+		47,
+		48
+	],
+	[ // 4s
+		41,
+		42,
+		43,
+		44
+	],
+	[ // 5s
+		37,
+		38,
+		39,
+		40
+	],
+	[ // 6s
+		33,
+		34,
+		35,
+		36
+	],
+	[ // 7s
+		29,
+		30,
+		31,
+		32
+	],
+	[ // 8s
+		25,
+		26,
+		27,
+		28
+	],
+	[ // 9s
+		21,
+		22,
+		23,
+		24
+	],
+	[ // 10s, Kings, Queens
+		5,
+		6,
+		7,
+		8,
+		9,
+		10,
+		11,
+		12,
+		13,
+		14,
+		15,
+		16,
+		17,
+		18,
+		19,
+		20
+	]
+];
+
+var deckHeight = 52;
