b628842628cb1a7ecaa27cf375f9a05c064945ce
MatthewMob <zwiddi011@ZWIDDI011_2017.hillsgrammar.nsw.edu.au>
2015-08-25 12:13:03 +1000

    Added functions to deck and dealer, have been updating but not commiting. Sorry!

--- scripts/core.js
+++ scripts/core.js
@@ -1,13 +1,71 @@
-// Set shadow size of deck relative to the amount of cards in the deck, the less cards, the smaller the shadow, creating a 3D effect.
-function setShadow() {
-	$("#deck").css("box-shadow", deckHeight/13+"px "+deckHeight/26+"px 5px black");
-}
-setShadow();
+/*
+	
+	This is the core JS file, it will run the game logic amongst other things
 
-var rulesSwitch = false;
-$("#rulesHeader").click(function() {
-	if (!rulesSwitch) {
-		$("#arrow").toggleClass("down");
+*/
+
+// 288px
+
+function Deck() {
+	this.list = cardList;
+	this.count = 52;
+	this.foundCard = false;
+	this.getVal = 0;
+	this.getRef = 0;
+
+	this.getCard = function() {
+
+		this.foundCard = false;
+		while (!this.foundCard) {
+			this.getVal = Math.floor((Math.random()*this.list.length)+1)
+			this.getRef = Math.floor((Math.random()*this.list[this.getVal].length)+0)
+			if (!this.list[this.getVal][this.getRef].used) {
+				this.list[this.getVal][this.getRef].used = true;
+				this.foundCard = true;
+				return this.list[this.getVal][this.getRef];
+			}
+		}
+
 	}
-	$("#rulesContent").slideToggle(400);
-});
+}
+
+function Player() {
+	this.hand = [];
+	this.handVal = 0;
+}
+
+function Dealer() {
+	this.hand = [];
+	this.handVal = 0;
+	this.handTotal = 1;
+	this.handPos = [null, "-=494px", 538] // 538px
+	this.delay = 1000;
+	var _this = this
+
+	this.drawCard = function() {
+
+		$("#dealerHand1").css("display", "block");
+		$("#dealerHand1").animate({
+			left: _this.handPos[_this.handTotal]
+		}, _this.delay, function() {
+			$("#dealerHand1").attr("src", "images/cards/5.png");
+			$("#dealerHand2").attr("src", "images/cards/b2fv.png");
+			$("#dealerHand2").css("display", "block");
+			$("#dealerHand2").animate({
+				left: dealer.handPos[2]
+			}, _this.delay, function() {
+				$(".button").slideDown(250);
+			});
+		});
+
+	}
+}
+
+var deck = new Deck();
+var player = new Player();
+var dealer = new Dealer();
+
+function startRound() {
+	deck = new Deck();
+	dealer = new Dealer();
+}
--- scripts/init.js
+++ scripts/init.js
@@ -1,243 +1,248 @@
+/*
+	
+	This is the file for initializing,
+	it includes DOM functions and variables to pre-define
+
+*/
+
 /* Make a 2D array that holds the cards as references - This array will be used to select cards on draws */
 var cardList = [
-	[ // Card Backs
-		"b2fv"
-	],
+	[], // Placeholder
 	[ // Aces
 		{
-			used: 0,
+			used: false,
 			ref: 1
 		},
 		{
-			used: 0,
+			used: false,
 			ref: 2
 		},
 		{
-			used: 0,
+			used: false,
 			ref: 3
 		},
 		{
-			used: 0,
+			used: false,
 			ref: 4
 		}
 	],
 	[ // 2s
 		{
-			used: 0,
+			used: false,
 			ref: 49
 		},
 		{
-			used: 0,
+			used: false,
 			ref: 50
 		},
 		{
-			used: 0,
+			used: false,
 			ref: 51
 		},
 		{
-			used: 0,
+			used: false,
 			ref: 52
 		}
 	],
 	[ // 3s
 		{
-			used: 0,
+			used: false,
 			ref: 45
 		},
 		{
-			used: 0,
+			used: false,
 			ref: 46
 		},
 		{
-			used: 0,
+			used: false,
 			ref: 47
 		},
 		{
-			used: 0,
+			used: false,
 			ref: 48
 		}
 	],
 	[ // 4s
 		{
-			used: 0,
+			used: false,
 			ref: 41
 		},
 		{
-			used: 0,
+			used: false,
 			ref: 42
 		},
 		{
-			used: 0,
+			used: false,
 			ref: 43
 		},
 		{
-			used: 0,
+			used: false,
 			ref: 44
 		}
 	],
 	[ // 5s
 		{
-			used: 0,
+			used: false,
 			ref: 37
 		},
 		{
-			used: 0,
+			used: false,
 			ref: 38
 		},
 		{
-			used: 0,
+			used: false,
 			ref: 39
 		},
 		{
-			used: 0,
+			used: false,
 			ref: 40
 		}
 	],
 	[ // 6s
 		{
-			used: 0,
+			used: false,
 			ref: 33
 		},
 		{
-			used: 0,
+			used: false,
 			ref: 34
 		},
 		{
-			used: 0,
+			used: false,
 			ref: 35
 		},
 		{
-			used: 0,
+			used: false,
 			ref: 36
 		}
 	],
 	[ // 7s
 		{
-			used: 0,
+			used: false,
 			ref: 29
 		},
 		{
-			used: 0,
+			used: false,
 			ref: 30
 		},
 		{
-			used: 0,
+			used: false,
 			ref: 31
 		},
 		{
-			used: 0,
+			used: false,
 			ref: 32
 		}
 	],
 	[ // 8s
 		{
-			used: 0,
+			used: false,
 			ref: 25
 		},
 		{
-			used: 0,
+			used: false,
 			ref: 26
 		},
 		{
-			used: 0,
+			used: false,
 			ref: 27
 		},
 		{
-			used: 0,
+			used: false,
 			ref: 28
 		}
 	],
 	[ // 9s
 		{
-			used: 0,
+			used: false,
 			ref: 21
 		},
 		{
-			used: 0,
+			used: false,
 			ref: 22
 		},
 		{
-			used: 0,
+			used: false,
 			ref: 23
 		},
 		{
-			used: 0,
+			used: false,
 			ref: 24
 		}
 	],
 	[ // 10s, Kings, Queens
 		{
-			used: 0,
+			used: false,
 			ref: 5
 		},
 		{
-			used: 0,
+			used: false,
 			ref: 6
 		},
 		{
-			used: 0,
+			used: false,
 			ref: 7
 		},
 		{
-			used: 0,
+			used: false,
 			ref: 8
 		},
 		{
-			used: 0,
+			used: false,
 			ref: 9
 		},
 		{
-			used: 0,
+			used: false,
 			ref: 10
 		},
 		{
-			used: 0,
+			used: false,
 			ref: 11
 		},
 		{
-			used: 0,
+			used: false,
 			ref: 12
 		},
 		{
-			used: 0,
+			used: false,
 			ref: 13
 		},
 		{
-			used: 0,
+			used: false,
 			ref: 14
 		},
 		{
-			used: 0,
+			used: false,
 			ref: 15
 		},
 		{
-			used: 0,
+			used: false,
 			ref: 16
 		},
 		{
-			used: 0,
+			used: false,
 			ref: 17
 		},
 		{
-			used: 0,
+			used: false,
 			ref: 18
 		},
 		{
-			used: 0,
+			used: false,
 			ref: 19
 		},
 		{
-			used: 0,
+			used: false,
 			ref: 20
 		}
 	]
 ];
 
-var deckHeight = 52;
-
-$(document).ready(function() {
-	// Set deck x coordinate to three quarters of the way across the page
-	$("#deck").css("left", $(window).width()-$(window).width()/4-$("#deck").width());
-	$("#rulesWrapper").css("left", $(window).width()/4-$("#rulesWrapper").width());
-	console.log("Rules Wrapper: "+$("#rulesWrapper").width()+"\nWindow Width: "+$(window).width());
+// When the user clicks on the rules, it will toggle a CSS class on/off that hides/displays the rules
+var rulesSwitch = false;
+$("#rulesHeader").click(function() {
+	if (!rulesSwitch) {
+		$("#arrow").toggleClass("down");
+	}
+	$("#rulesContent").slideToggle(400);
 });
